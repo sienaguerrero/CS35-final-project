7746ac9deb77ba07f0c6cd5f2d846f055e2b161e
Hannah Slocumb <hslocumb@g.hmc.edu>
2017-04-24 17:34:55 -0700

    xus tarp

--- pratplot.py
+++ pratplot.py
@@ -26,10 +26,10 @@ def convertDate(tempList):
     newList = []
     for tup in tempList:
         if "-" in tup[0]:
-            newDate = datetime.datetime.strptime(tup[0], '%Y-%m-%d').strftime('%m/%d/%Y')
+            newDate = datetime.datetime.strptime(tup[0], '%Y-%m-%d').strftime('%Y')
             newList.append((newDate, tup[1]))
         else:
-            newDate = datetime.datetime.strptime(tup[0], '%d-%m-%y').strftime('%m/%d/%Y')
+            newDate = datetime.datetime.strptime(tup[0], '%d/%m/%y').strftime('%Y')
             newList.append(tup[0],tup[1])
 
     return newList
@@ -41,13 +41,15 @@ def doAllTheWork(filename = "GlobalLandTemperaturesByCountry.csv"):
     dateList = []
     tList = []
     for tup in N:
-        if tup[1].isdigit():
+        try:
+            tList.append(float(tup[1]))
             dateList.append(tup[0])
-            tList.append(int(tup[1]))
+        except:
+            continue
     output_file("pratplot.html")
     p = figure(plot_width=800, plot_height=250, x_axis_type="datetime")
 
-    p.line(dateList, tList, color='navy', alpha=0.5)
+    p.circle(dateList, tList, radius=0.1, alpha=0.5)
 
-    p.xaxis.formatter=DatetimeTickFormatter( years = ['%d/%m/%Y'], months = ['%d/%m/%Y'], days = ['%d/%m/%Y'] )
+    p.xaxis.formatter=DatetimeTickFormatter( years = ['%Y'])
     show(p)
